2c54f6412fcc9a40b488ca5b4b31b5adaf288c2f
mjghr <mjghrwork@gmail.com>
2025-03-23 16:44:09 +0330

    added post.html also moved some constants to const.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
+
+	"github.com/computer-technology-4022/quera-clone/internal/config"
+	handler "github.com/computer-technology-4022/quera-clone/internal/handlers"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("../web/templates/index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w,nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../web/static"))))
-	http.HandleFunc("/", handler)
-	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	http.Handle(config.StaticRouter, http.
+		StripPrefix(config.StaticRouter, http.FileServer(http.Dir(config.StaticRouterDir))))
+	http.HandleFunc("/", handler.WelcomeHandler)
+	http.ListenAndServe(config.ServerPort, nil)
 }

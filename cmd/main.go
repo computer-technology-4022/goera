c0bd0ae16977dae1686e7f41c5e4c23da9fa09ea
mjghr <mjghrwork@gmail.com>
2025-03-23 15:20:52 +0330

    added a simple router and loaded index.html

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+
+	tmpl, err := template.ParseFiles("../web/templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w,nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}

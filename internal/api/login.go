4a5f9273fe78f1655472cabb403e47dc17a37f14
mjghr <mjghrwork@gmail.com>
2025-03-30 17:37:16 +0330

    added login handler for login route of api

---
+++ internal/api/login.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+	"github.com/computer-technology-4022/goera/internal/auth"
+	"github.com/computer-technology-4022/goera/internal/database"
+	"github.com/computer-technology-4022/goera/internal/models"
+)
+
+type loginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Methode not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var loginRequest loginRequest
+	if err := json.NewDecoder(r.Body).Decode(&loginRequest); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db := database.GetDB()
+	var user models.User
+	if result := db.Where("username = ?", loginRequest.Username).First(&user); result.Error != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	if !auth.CheckPasswordHash(loginRequest.Password, user.Password) {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(168 * time.Hour)
+	token, err := auth.GenerateJWT(user.ID)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	auth.SetTokenCookie(w, token, "token", expirationTime)
+
+	user.Password = ""
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"user":  user,
+		"token": token,
+	})
+}

bcf2a3977096a565db52f893dec5fccbc904fe1a
mjghr <mjghrwork@gmail.com>
2025-03-30 17:39:53 +0330

    initialized the db and added signup handler

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/computer-technology-4022/goera/internal/api"
 	"github.com/computer-technology-4022/goera/internal/config"
+	"github.com/computer-technology-4022/goera/internal/database"
 	handler "github.com/computer-technology-4022/goera/internal/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	database.InitDB()
 	r := mux.NewRouter()
-
 	r.Handle(config.StaticRouter, http.
 		StripPrefix(config.StaticRouter, http.FileServer(http.Dir(config.StaticRouterDir))))
 	r.HandleFunc("/", handler.WelcomeHandler)
@@ -21,9 +23,9 @@ func main() {
 	r.HandleFunc("/question", handler.QuestionHandler)
 	fmt.Println("Server is running on http://localhost:8080")
 
-	// s := r.PathPrefix("/api").Subrouter()
-	// s.HandleFunc("/login", api.LoginHandler).Methods("GET", "POST")
-	// s.HandleFunc("/register", api.RegisterHandler).Methods("GET", "POST")
+	s := r.PathPrefix("/api").Subrouter()
+	s.HandleFunc("/login", api.LoginHandler).Methods("GET", "POST")
+	s.HandleFunc("/register", api.RegisterHandler).Methods("GET", "POST")
 	// s.HandleFunc("/user/{ID}", auth.JWTMiddleware(api.UsersHandler)).Methods("GET", "POST")
 
 	http.Handle("/", r)
---
+++ internal/api/register.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/computer-technology-4022/goera/internal/auth"
+	"github.com/computer-technology-4022/goera/internal/database"
+	"github.com/computer-technology-4022/goera/internal/models"
+)
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Methode not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	hasedPassword, err := auth.HashPassword(user.Password)
+	if err != nil {
+		http.Error(w, "failed to hash password", http.StatusInternalServerError)
+		return
+	}
+
+	user.Password = hasedPassword
+
+	db := database.GetDB()
+	if result := db.Create(&user); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	token, err := auth.GenerateJWT(user.ID)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	expirationTime := time.Now().Add(168 * time.Hour)
+	auth.SetTokenCookie(w, token, "token", expirationTime)
+
+	user.Password = ""
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"user":  user,
+		"token": token,
+	})
+}

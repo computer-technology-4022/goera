b902be5aadd491a9639e86e61e01eb3aa0d59c6a
nima1313 <nimaalighardashi@gmail.com>
2025-04-02 01:34:27 +0330

    added profile id gorilla

--- internal/handlers/profile_handler.go
+++ internal/handlers/profile_handler.go
@@ -1,11 +1,17 @@
 package handler
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Println("Profile ID:", id)
 	tmpl, err := template.ParseFiles("web/templates/profile.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)

1195cf33eaf64c905034355ebec9b8e517b6f477
mjghr <mjghrwork@gmail.com>
2025-04-10 13:17:52 +0330

    done changing db to postgres

--- internal/config/const.go
+++ internal/config/const.go
@@ -4,6 +4,14 @@ const (
 	ServerPort      = ":5000"
 	StaticRouterDir = "web/static"
 	StaticRouter    = "/static/"
+
+	// Database configuration
+	DBHost     = "localhost"
+	DBUser     = "goera_user"
+	DBPassword = "GoerA2225*&"               // Empty password if you haven't set one
+	DBName     = "goera"
+	DBPort     = "5432"
+	DBSSLMode  = "disable"
 )
 
 var ProtectedPrefixes = []string{
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"fmt"
+
+	"github.com/computer-technology-4022/goera/internal/config"
 	"github.com/computer-technology-4022/goera/internal/models"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -10,28 +13,24 @@ var DB *gorm.DB
 
 func InitDB() error {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("goera.db"), &gorm.Config{})
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		config.DBHost, config.DBUser, config.DBPassword, config.DBName, config.DBPort, config.DBSSLMode)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return err
 	}
 
-	err = DB.AutoMigrate(&models.User{})
-	if err != nil {
-		return err
-	}
-
-	DB.Model(&models.User{}).Where("role = ''").Update("role", models.RegularRole)
-
-	err = DB.AutoMigrate(&models.Question{})
-	if err != nil {
-		return err
-	}
-	err = DB.AutoMigrate(&models.Submission{})
-	if err != nil {
-		return err
-	}
+	models.MigrateQuestion(DB)
 
 	return nil
+}
+
+func CloseDB() error {
+	db, err := DB.DB()
+	if err != nil {
+		return err
+	}
+	return db.Close()
 }
 
 func GetDB() *gorm.DB {

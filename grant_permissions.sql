b116d0d044d442867216c4c7e91bec20048b1096
mjghr <mjghrwork@gmail.com>
2025-04-28 17:59:40 +0330

    removed files that were not needed

--- grant_permissions.sql
+++
@@ -1,5 +0,0 @@
--- Grant permissions to goera_user
-GRANT ALL ON SCHEMA public TO goera_user;
-GRANT ALL ON ALL TABLES IN SCHEMA public TO goera_user;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO goera_user;
-ALTER USER goera_user CREATEDB; 
